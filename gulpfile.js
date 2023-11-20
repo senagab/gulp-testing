ab5faf59edc2cc63ed153999075aa796bc99badc
senagab <senagab@gmail.com>
2023-11-19 22:12:35 -0400

    feat: minificação de imagens, compressão de javascrip, gulp obfuscate, gulp uglifyt

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
+const imagemin = require('gulp-imagemin');
+
+function comprimeImagens() {
+    return gulp.src('./source/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'))
+}
+
+function comprimeJavascript () {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest('./build/scripts'))
+}
 
 function compilaSass() {
     return gulp.src('./source/styles/main.scss')
@@ -12,28 +28,8 @@ function compilaSass() {
         .pipe(gulp.dest('./build/styles'));
 }
 
-function funcaoPadrao (callback) {
-    setTimeout(function () {
-        console.log('executando via gulp');
-        callback();
-    }, 2000)
-}
-
-function dizOi(callback) {
-    setTimeout (function () {
-        console.log('oi gulp');
-        dizTchau();
-        callback();
-    }, 1000)
-}
-
-function dizTchau() {
-    console.log('tchau gulp');
-}
-
-exports.default = gulp.series(funcaoPadrao, dizOi);
-exports.dizOi = dizOi;
-exports.sass = compilaSass;
-exports.watch = function () {
+exports.default = function () {
     gulp.watch('./source/styles/*.scss', {ignoreInitial: false }, gulp.series(compilaSass));
-}
+    gulp.watch('./source/scripts/*.js', {ignoreInitial: false }, gulp.series(comprimeJavascript));
+    gulp.watch('./source/images/*', {ignoreInitial: false }, gulp.series(comprimeImagens));
+};
